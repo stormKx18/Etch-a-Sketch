35d73e60e5ef149ea07ba5f56d619754416e5b76
hirian <vicjaime922@gmail.com>
2024-05-30 13:23:43 -0600

    Add button to resize grid

--- javascript.js
+++ javascript.js
@@ -1,34 +1,61 @@
 console.log('Etch-a-Sketch');
 
+let rows = 16; //Default grid size
+const MAX_ROWS=100; //Max number of rows
+
 /* Create Grid*/
-let rows = 10;
-let cols = 10;
+function drawGrid(rows) {
+    let grid = document.querySelector('.grid');
+    grid.innerHTML=""; //Clear previous grid
 
-let grid = document.querySelector('.grid');
+    for (let j = 0; j < rows; j++) { //Create rows
+        const row = document.createElement("div");
+        row.classList.add("row");
 
-for (let j = 0; j < rows; j++) { //Create rows
-    const row = document.createElement("div");
-    row.classList.add("row");
+        for(let i=0; i < rows; i++){ //Create cells
+            const cell = document.createElement("div");
+            cell.classList.add("cell");
+            row.appendChild(cell)
+        }
 
-    for(let i=0; i < cols; i++){ //Create cells
-        const cell = document.createElement("div");
-        cell.classList.add("cell");
-        row.appendChild(cell)
+        grid.appendChild(row);
     }
 
-    grid.appendChild(row);
+
+    /* Add event listeners */
+    grid.addEventListener('mouseover',(event)=>{
+        let target = event.target;
+        
+        target.classList.add("painted");
+
+        if(target.classList.contains('row') || target.classList.contains('grid')){
+            target.classList.remove("painted");
+        }
+    })
 }
 
+/* Clear Grid */
+let btnGrid = document.querySelector(".createGrid");
 
-/* Add event listeners */
-grid.addEventListener('mouseover',(event)=>{
-    let target = event.target;
-    
-    target.classList.add("painted");
+btnGrid.addEventListener('click',(event)=>{
+    console.log('Clear Grid');
+    drawGrid(rows);
+});
 
-    if(target.classList.contains('row') || target.classList.contains('grid')){
-        target.classList.remove("painted");
+/* Resize Grid */
+let btnResize = document.querySelector('.resizeGrid');
+
+btnResize.addEventListener('click',(event)=>{
+    console.log('Resize Grid');
+
+    const userInput = +prompt('Please enter grid size (max:100):', '16');
+
+    if (userInput !== null) {
+        rows = Math.min(userInput,MAX_ROWS); //Limit to max rows
+        drawGrid(rows);
     }
 })
 
 
+//Create initial Grid
+drawGrid(rows);
